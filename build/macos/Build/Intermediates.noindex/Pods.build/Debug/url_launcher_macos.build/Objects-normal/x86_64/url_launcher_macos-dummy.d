16813ded2578b73e1b4f947993754a277eee189f
Edwin Wilson <ewilson@altayer.com>
2021-09-29 12:35:37 +0400

    making release versions

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/Objects-normal/x86_64/url_launcher_macos-dummy.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/ewilson/Documents/Trial\ Projects/untitled/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-dummy.m \
+  /Users/ewilson/Documents/Trial\ Projects/untitled/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/module.modulemap \
+  /Users/ewilson/Documents/Trial\ Projects/untitled/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/dyld.modulemap
